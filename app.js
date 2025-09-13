71c2bf06f7f55cb34d098421db8822b9d1849bd1
unknown <patrick.s.martins02@gmail.com>
2025-09-13 17:46:21 -0300

    adicionado o return na divisão

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get("/calculadora", (req, res)=>{
             case "divisao":
                 // tratamento de erro para divisão por 0
                 if(numeroDois === 0 ){    
-                    res.status(400).send(`Não é possivel fazer divisão por zero!`)
+                   return res.status(400).send(`Não é possivel fazer divisão por zero!`)
                 }
                 
                 resultado = numeroUm / numeroDois;
