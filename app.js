8bd764d16e0f79ed045c7fb5d5fe93fe43ec4a6b
pmartins-dev <patrick.s.martins02@gmail.com>
2025-09-13 12:26:20 -0300

    Ex01 em produção

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.get("/soma/:numUm/:numDois", (req, res)=>{
+
+    try {
+        const {numUm, numDois} = req.params;
+        if(isNaN(numUm) || numUm == undefined || numUm == null || isNaN(numDois) || numDois == undefined || numDois == null){
+            return res.status(400).send(`Os valores recebidos não são números ou estão incompletos.`)
+        }
+        const numeroUm = parseFloat(numUm);
+        const numeroDois = parseFloat(numDois);
+        const soma = numeroUm + numeroDois;
+        res.status(200).send(`Soma realizada dos números ${numeroUm} + ${numeroDois} = ${soma}`)
+    } catch (error) {
+        console.error(`Erro reportado:`, error);
+        res.status(500).send(`Erro interno no servidor!`);
+    }
+
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
---
+++ task.md
@@ -0,0 +1,9 @@
+Tarefas:
+1. Crie um projeto Express com quatro rotas GET, cada uma representando uma operação matemática: adição, subtração, multiplicação e divisão. 
+
+Cada rota deve capturar os parâmetros numUm e numDois da URL, realizar a operação correspondente e retornar o resultado no formato de texto simples.
+Trate possíveis erros, como divisão por zero, e retorne mensagens adequadas.
+
+Exemplo de chamada:
+GET /soma/5/10 deve retornar:
+Resultado da soma: 15
